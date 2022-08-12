670ecf88bd3d628c9ce9229a186470fdd771b519
walrus22 <104523755+walrus22@users.noreply.github.com>
2022-08-12 22:28:16 +0900

    1123

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 npm run build
-aws s3 sync build s3://webtoonguru-build-jjy/build
+aws s3 sync build s3://webtoonguru-build-jjy
 aws cloudfront create-invalidation \
     --distribution-id E2PITSB50RFHQS \
     --paths "/*"
